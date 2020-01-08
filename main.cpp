0a9ec739ba9281618ef983b3c7ed2b917c1e0ecb
Lu <chris.lu@syccuxas01.pcc.edu>
2020-01-07 17:27:31 -0800

    hello worlded

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "main.h"
+
+using namespace std;
+
+int main() {
+string product_name;
+int cost;
+	cout << "look a lovely shopper program" << endl;
+	cout << "input product name: " << endl;
+	cin >> product_name;
+
+	cout << "input product cost: " << endl;
+	cin >> cost;
+
+	cout << "Product name is " << product_name << " cost is " << cost << endl;
+
+return 0;
+}
+
