59aaba88b108a72e88f051c775a207b3e1a35699
Lu <chris.lu@syccuxas01.pcc.edu>
2020-01-08 17:27:57 -0800

    exiting loop established, numbers changed to floats

--- main.cpp
+++ main.cpp
@@ -4,15 +4,42 @@ using namespace std;
 
 int main() {
 string product_name;
-int cost;
-	cout << "look a lovely shopper program" << endl;
-	cout << "input product name: " << endl;
-	cin >> product_name;
+float cost; 
+float total = 0; //initializing it here so it only starts zero 
+bool done = false;
+while (done == false) //we're using a while loop here because it is a perfect toggle
+{
+cout.setf(ios::fixed, ios::floatfield);
+cout.setf(ios::showpoint);
+cout.precision(2);
 
-	cout << "input product cost: " << endl;
-	cin >> cost;
+cout << "its a lovely shopper program" << endl;
+cout << "input product name: " << endl;
+cin >> product_name;
 
-	cout << "Product name is " << product_name << " cost is " << cost << endl;
+cout << "input product cost: " << endl;
+cin >> cost;
+total = total + cost;
+cout << "Product name is " << product_name << " cost is " << cost << endl;
+cout << "total is " << total << endl;
+cout << "Are you done? (y/n)";
+string usrInput;
+cin >> usrInput;
+if(usrInput == "y")
+{
+	done = true;
+} else 
+{
+done = false;
+}
+
+
+}
+
+//add decimales and show them
+//needs to QA for good types
+//upon grand total, needs to be copmleted
+
 
 return 0;
 }
