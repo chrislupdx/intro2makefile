ae391f02074c71d1df070891e63f39b81d00bd2e
Lu <chris.lu@syccuxas01.pcc.edu>
2020-01-08 18:09:30 -0800

    added quality controls for both inputs

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 
 int main() {
-string product_name;
+int const MAX_LEN = 100;
+char product_name[MAX_LEN] ;
 float cost; 
 float total = 0; //initializing it here so it only starts zero 
 bool done = false;
@@ -16,17 +17,37 @@ cout.precision(2);
 cout << "its a lovely shopper program" << endl;
 cout << "input product name: " << endl;
 cin >> product_name;
+while (!cin)
+{
+	//clear the error code to return cin back to a working state
+	cin.clear();
+	cin.ignore(100, '\n');
+
+	cout << "Illegal value, try again";
+	cin >> product_name;
+}
+	cin.ignore(100, '\n');
 
 cout << "input product cost: " << endl;
 cin >> cost;
+while (!cin)
+{
+	cin.clear();
+	cin.ignore(100, '\n');
+
+	cout << "illegal value, try again";
+	cin >> cost;
+}
+
+
 total = total + cost;
 cout << "Product name is " << product_name << " cost is " << cost << endl;
-cout << "total is " << total << endl;
 cout << "Are you done? (y/n)";
 string usrInput;
 cin >> usrInput;
 if(usrInput == "y")
 {
+	cout << "total is " << total << endl;
 	done = true;
 } else 
 {
@@ -35,11 +56,7 @@ done = false;
 
 
 }
-
-//add decimales and show them
 //needs to QA for good types
-//upon grand total, needs to be copmleted
-
 
 return 0;
 }
