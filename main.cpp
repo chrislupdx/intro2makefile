2cae63fe4b2e9a7368b3cc99553e3bcab7733a97
Lu <chris.lu@syccuxas01.pcc.edu>
2020-01-09 19:10:21 -0800

    done

--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
 #include "main.h"
 
-using namespace std;
-
 int main() {
 int const MAX_LEN = 100;
 char product_name[MAX_LEN] ;
@@ -10,53 +8,48 @@ float total = 0; //initializing it here so it only starts zero
 bool done = false;
 while (done == false) //we're using a while loop here because it is a perfect toggle
 {
-cout.setf(ios::fixed, ios::floatfield);
-cout.setf(ios::showpoint);
-cout.precision(2);
+std::cout.setf(std::ios::fixed, std::ios::floatfield);
+std::cout.setf(std::ios::showpoint);
+std::cout.precision(2);
 
-cout << "its a lovely shopper program" << endl;
-cout << "input product name: " << endl;
-cin >> product_name;
-while (!cin)
-{
-	//clear the error code to return cin back to a working state
-	cin.clear();
-	cin.ignore(100, '\n');
+std::cout << "its a lovely shopper program" << std::endl;
+std::cout << "input product name: " << std::endl;
 
-	cout << "Illegal value, try again";
-	cin >> product_name;
-}
-	cin.ignore(100, '\n');
+std::cin.get(product_name, MAX_LEN, '\n');
 
-cout << "input product cost: " << endl;
-cin >> cost;
-while (!cin)
-{
-	cin.clear();
-	cin.ignore(100, '\n');
+	while(!std::cin) //can we actually do that?
+	{	
+		std::cout << "illegal product name" << std::endl;
+		std::cin.clear();
+		std::cin.ignore(100, '\n');
+		std::cin.get(product_name, MAX_LEN, '\n');
+	}
 
-	cout << "illegal value, try again";
-	cin >> cost;
-}
-
-
+std::cout << "input product cost: " << std::endl;
+std::cin >> cost;
+	while (!std::cin)
+	{
+		std::cin.clear();
+		std::cin.ignore(100, '\n');
+		std::cout << "illegal cost value, try again" << std::endl;
+		std::cin >> cost;
+	}
 total = total + cost;
-cout << "Product name is " << product_name << " cost is " << cost << endl;
-cout << "Are you done? (y/n)";
-string usrInput;
-cin >> usrInput;
-if(usrInput == "y")
-{
-	cout << "total is " << total << endl;
-	done = true;
-} else 
-{
-done = false;
+std::cout << "Product name is " << product_name << " cost is " << cost << std::endl;
+std::cout << "Are you done? (y/n)";
+std::string usrInput;
+std::cin >> usrInput;
+	if(usrInput == "y")
+	{
+		std::cout << "total is " << total << std::endl;
+		done = true;
+	} else 
+	{
+		done = false;
+	}
+std::cin.clear();
+std::cin.ignore(100, '\n');
 }
-
-
-}
-//needs to QA for good types
 
 return 0;
 }
